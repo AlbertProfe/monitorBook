c439e5026abf96dfe5d8f146e32edb2c127369dc
AlbertProfe <albertprofe@gmail.com>
2023-11-03 13:26:15 +0100

    v2.1 crud project with DB-H2 lombok

--- demoJavaBoot2/src/main/java/com/example/demo/model/Book.java
+++ demoJavaBoot2/src/main/java/com/example/demo/model/Book.java
@@ -2,8 +2,14 @@ package com.example.demo.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data // generates getters, setters, equals, hashCode, and toString methods
+@NoArgsConstructor // generates a no-args constructor
+@AllArgsConstructor // generates a constructor with all arguments
 public class Book {
 
     @Id
@@ -13,55 +19,4 @@ public class Book {
     private String language;
     private String author;
 
-
-
-    public Book(String id, String title, int pages, String language, String author) {
-        this.id = id;
-        this.title = title;
-        this.pages = pages;
-        this.language = language;
-        this.author = author;
-    }
-
-        public Book (){}
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public int getPages() {
-        return pages;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setPages(int pages) {
-        this.pages = pages;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
 }
